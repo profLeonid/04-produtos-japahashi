7b85b50d03efa43f3dc78e4ee4684c7f87c4cb16
ISRAEL FUJIMOTO <25203688@dsn127m1246727.cfp127aluno.local>
2026-02-24 16:51:22 -0300

    Adicionando funções de codigo, produto e quantidade

--- main.js
+++ main.js
@@ -3,9 +3,36 @@
 function adicionarProduto(){
     const produto = document.getElementById('produto')
     const lista = document.getElementById('lista')
-    const span = document.createElement('span')
-    span.textContent = produto.value
-    span.className = 'bg-blue-200 px-8 py-2'
-    lista.appendChild(span)
+    const spanProduto = document.createElement('span')
+    const spanCodigo = document.createElement('span')
+    const spanQuantidade = document.createElement('span')
+    const codigo = document.getElementById('codigo')
+    const quantidade = document.getElementById('quantidade')
+   
     
+    if(produto.value == "" || codigo.value == "" || quantidade.value == ""){
+        alert("ERRO: Insira algo nos campos")
+
+    }else{
+        spanProduto.textContent = produto.value
+        spanCodigo.textContent = codigo.value
+        spanQuantidade.textContent = quantidade.value
+        
+        
+        spanProduto.className = 'bg-blue-200 px-8 py-2'
+        spanCodigo.className = 'bg-blue-200 px-8 py-2'
+        spanQuantidade.className = 'bg-blue-200 px-8 py-2'
+
+        lista.appendChild(spanProduto)
+        lista.appendChild(spanCodigo)
+        lista.appendChild(spanQuantidade)
+
+        produto.value = ""
+        codigo.value = ""
+        quantidade.value = ""
+    }
+
+    
+
+
 }
