aad0a0dffd1432385dacae00540c06e1ac70ff17
ISRAEL FUJIMOTO <25203688@dsn127m1246727.cfp127aluno.local>
2026-02-24 15:04:19 -0300

    Adicionando aula produtos

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function adicionarProduto(){
+    const produto = document.getElementById('produto')
+    const lista = document.getElementById('lista')
+    const span = document.createElement('span')
+    span.textContent = produto.value
+    span.className = 'bg-blue-200 px-8 py-2'
+    lista.appendChild(span)
+    
+}
